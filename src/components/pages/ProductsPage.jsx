cfa8e5ed7a31c1a31f10de5895340d3217801565
Omerkugel12 <omerkugel12@gmail.com>
2024-06-25 22:03:18 +0300

    refactoring-filter-by-name

--- src/components/pages/ProductsPage.jsx
+++ src/components/pages/ProductsPage.jsx
@@ -1,29 +1,61 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link, useNavigate, useOutletContext } from "react-router-dom";
+import {
+  Link,
+  useNavigate,
+  useOutletContext,
+  useSearchParams,
+} from "react-router-dom";
 
 const PRODUCTS_URL = "http://localhost:3000/api/product";
-// const URL = "http://localhost:5173/products";
+const URL_web = "http://localhost:5173/products";
 
 function ProductsPage() {
   const [products, setProducts] = useOutletContext();
   const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
     async function getProducts() {
+      const options = {
+        params: {
+          name: searchParams.get("name"),
+        },
+      };
       try {
-        const { data } = await axios.get(PRODUCTS_URL);
+        const { data } = await axios.get(PRODUCTS_URL, options);
         setProducts(data);
       } catch (error) {
         console.log(error);
       }
     }
     getProducts();
-  }, []);
+  }, [searchParams]);
+
+  // const handleSearchChange = (e) => {
+  //   setSearchParams({ name: e.target.value }, { replace: true });
+  // };
+
+  function handleFilterChange(ev) {
+    const inputName = ev.target.name;
+    const value = ev.target.value;
+    searchParams.set(inputName, value);
+    setSearchParams(searchParams);
+  }
 
   return (
     <div className="px-5 pt-4">
       <h1 className="text-indigo-700 font-bold text-3xl ">Products</h1>
+      <div>
+        <h3>search by name</h3>
+        <input
+          type="text"
+          name="name"
+          value={searchParams.get("name" || "")}
+          onChange={handleFilterChange}
+          placeholder="Enter product name..."
+        />
+      </div>
       <ul className="flex flex-wrap gap-10 ">
         {products.map((product) => {
           return (
