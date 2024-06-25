667855610de1430799964a993f59abcf43530f42
Omerkugel12 <omerkugel12@gmail.com>
2024-06-26 00:26:34 +0300

    refactoring-filter-by-minPrice

--- src/components/pages/ProductsPage.jsx
+++ src/components/pages/ProductsPage.jsx
@@ -60,7 +60,7 @@ function ProductsPage() {
         <input
           type="number"
           name="minPrice"
-          value={searchParams.get("minPrice") || 0}
+          value={searchParams.get("minPrice") || ""}
           onChange={handleFilterChange}
           placeholder="Enter product minimum price..."
         />
@@ -70,7 +70,7 @@ function ProductsPage() {
         <input
           type="number"
           name="maxPrice"
-          value={searchParams.get("MaxPrice") || 5000}
+          value={searchParams.get("maxPrice") || 1500}
           onChange={handleFilterChange}
           placeholder="Enter product maximum price..."
         />
@@ -111,3 +111,117 @@ function ProductsPage() {
 }
 
 export default ProductsPage;
+
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import {
+//   Link,
+//   useNavigate,
+//   useOutletContext,
+//   useSearchParams,
+// } from "react-router-dom";
+
+// const PRODUCTS_URL = "http://localhost:3000/api/product";
+
+// function ProductsPage() {
+//   const [products, setProducts] = useOutletContext();
+//   const navigate = useNavigate();
+//   const [searchParams, setSearchParams] = useSearchParams();
+
+//   useEffect(() => {
+//     async function getProducts() {
+//       const options = {
+//         params: {
+//           name: searchParams.get("name"),
+//           category: searchParams.get("category"),
+//           minPrice: searchParams.get("minPrice"),
+//           maxPrice: searchParams.get("maxPrice"),
+//         },
+//       };
+//       try {
+//         const { data } = await axios.get(PRODUCTS_URL, options);
+//         setProducts(data);
+//       } catch (error) {
+//         console.error("Error fetching products:", error);
+//       }
+//     }
+//     getProducts();
+//   }, [searchParams, setProducts]);
+
+//   function handleFilterChange(ev) {
+//     const inputName = ev.target.name;
+//     const value = ev.target.value;
+//     searchParams.set(inputName, value);
+//     setSearchParams(searchParams);
+//   }
+
+//   return (
+//     <div className="px-5 pt-4">
+//       <h1 className="text-indigo-700 font-bold text-3xl">Products</h1>
+//       <div>
+//         <h3>Search by name</h3>
+//         <input
+//           type="text"
+//           name="name"
+//           value={searchParams.get("name") || ""}
+//           onChange={handleFilterChange}
+//           placeholder="Enter product name..."
+//         />
+//       </div>
+//       <div>
+//         <h3>Minimum price</h3>
+//         <input
+//           type="number"
+//           name="minPrice"
+//           value={searchParams.get("minPrice") || 0}
+//           onChange={handleFilterChange}
+//           placeholder="Enter product minimum price..."
+//         />
+//       </div>
+//       <div>
+//         <h3>Maximum price</h3>
+//         <input
+//           type="number"
+//           name="maxPrice"
+//           value={searchParams.get("maxPrice") || 5000}
+//           onChange={handleFilterChange}
+//           placeholder="Enter product maximum price..."
+//         />
+//       </div>
+//       <div>
+//         <h3>Select category</h3>
+//         <select
+//           name="category"
+//           value={searchParams.get("category") || ""}
+//           onChange={handleFilterChange}
+//           required
+//         >
+//           <option value="">Select category</option>
+//           <option value="Accessories">Accessories</option>
+//           <option value="Home appliances">Home Appliances</option>
+//           <option value="Electronics">Electronics</option>
+//           <option value="Smart Home">Smart Home</option>
+//           <option value="Automotive">Automotive</option>
+//           <option value="Wearables">Wearables</option>
+//           <option value="Health">Health</option>
+//         </select>
+//       </div>
+//       <ul className="flex flex-wrap gap-10">
+//         {products.map((product) => (
+//           <Link key={product._id} to={product._id}>
+//             <li className="w-80 p-10 shadow-2xl shadow-500-grey flex-col space-y-6 justify-">
+//               <p>{product.name}</p>
+//               <p>{product.price}$</p>
+//               <p>{product.category}</p>
+//             </li>
+//           </Link>
+//         ))}
+//       </ul>
+//       <button onClick={() => navigate("create", { replace: true })}>
+//         Add product
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default ProductsPage;
